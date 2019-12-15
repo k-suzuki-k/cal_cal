4fecbdd0a71bc4a0de69609e4262209506cb0bba
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-12-15 18:22:02 +0900

    Add toggle feature (calender to todo's_form)

--- app/assets/javascripts/calender.js
+++ app/assets/javascripts/calender.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
       right: 'today prev,next'
     },
 
+    //カレンダーの日付をクリックするとその日付のtodoを抽出する
     dayClick: function(date) {
-
       $.ajax({
         url: '/todos/search',
         type: 'GET',
@@ -36,12 +36,32 @@ $(document).ready(function(){
 
   });
 });
-
-
 $(document).on('turbolinks:load', function() {
     $('#calendar').fullCalendar({});
 });
 
 $(document).on('turbolinks:before-cache', function() {
     $('#calendar').empty();
+});
+
+
+//カレンダーの日付をダブルクリックすると入力フォームが表示される
+$(document).ready(function(){
+  $(document).on("dblclick", ".fc-day", function(){
+
+    $('.todos_new_form').toggleClass('is_hide');
+    $('.calendar_view').toggleClass('is_hide');
+
+    var dblclick_day = $(this).data('date');
+    $('#todo_day').val(dblclick_day);
+
+  });
+});
+
+//入力フォームのボタンをクリックするとカレンダーへ戻る
+$(document).ready(function(){
+  $(document).on("click", ".to_calender_btn", function(){
+    $('.todos_new_form').toggleClass('is_hide');
+    $('.calendar_view').toggleClass('is_hide');
+  });
 });
