c5a1ae1c941be1266962b9889d328dddef42f7e5
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-12-17 22:41:23 +0900

    Add post new&create feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,35 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
-  def edit
+  def new
+    @post = Post.new
   end
 
   def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+
+    if post.save
+      redirect_to post_path(post)
+    else
+      render :new
+    end
+  end
+
+  def edit
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id, :day, :title, :content)
   end
 end
