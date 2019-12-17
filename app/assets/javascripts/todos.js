7862a3aae5dea16d77e9208b5b8f2947086ca154
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-12-17 15:53:59 +0900

    Add todo's update feature

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -9,16 +9,34 @@ $(document).on('turbolinks:load', function() {
     increase_direction:'up',
     disable_keyboard_mobile: true});
 });
+$(document).ready(function(){
+  $(document).on("click", ".timepicker", function() {
+    $('.timepicker').timepicki({
+      show_meridian:false,
+      min_hour_value:0,
+      max_hour_value:23,
+      step_size_minutes:5,
+      overflow_minutes:true,
+      increase_direction:'up',
+      disable_keyboard_mobile: true});
+  });
+});
 
 
 //todoをクリックすると詳細フォームが表示される
 $(document).ready(function(){
   $(document).on("click", ".todo_row", function(){
 
-    $('.todo_edit_view').removeClass('is_hide');
-    $('.todos_new_form').addClass('is_hide');
-    $('.calendar_view').addClass('is_hide');
-    $('.todo_show_view').addClass('is_hide');
+    var clicked_todo_id = this.id.split(/todo_/)[1];
+    var url_id = location.href.split('/')[4];
 
+    $.ajax({
+      url: '/calender/' + url_id + '/todos/show',
+      type: 'GET',
+      data: ('keyword=' + clicked_todo_id),
+      processData: false,
+      contentType: false,
+      dataType: 'script'
+    });
   });
 });
