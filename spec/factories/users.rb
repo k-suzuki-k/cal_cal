62fe636c7d3ecc70b01f3c0d2a35140f844d5497
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-12-21 14:48:54 +0900

    Add User model test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'tester@gmail' }
+    name { '' }
+    profile { '' }
+    password { 'password' }
+
+    trait :no_email do
+      email { '' }
+    end
+
+    trait :no_password do
+      password { '' }
+    end
+
+    trait :not_enough_password do
+      password { '12345' }
+    end
+
   end
 end
