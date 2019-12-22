187453b2b3343a8e86a2339b395fd802b3802425
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-12-22 10:11:00 +0900

    Fix users before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
   # ログインユーザのみ自身の情報を編集、更新できる
-  include CommonActions
   before_action :correct_user, only: [:edit, :update]
+  def correct_user
+    user = User.find(params[:id])
+    unless user == current_user
+      redirect_to calender_path(current_user)
+    end
+  end
 
   def edit
     @user = User.find(params[:id])
