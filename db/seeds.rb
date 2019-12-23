e634dc6f2a7e3c9cd0c593a1afdfa904430e1e2e
k-suzuki-k <whtecHko1000cJKxEE56@gmail.com>
2019-12-23 19:36:50 +0900

    Add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# factory_botでテストデータを作成
+require 'factory_bot_rails'
+include FactoryBot::Syntax::Methods
+FactoryBot.definition_file_paths = [ Rails.root.join('spec', 'factories') ]
+
+10.times do
+  create(:seed_user, :with_todos_and_posts)
+end
